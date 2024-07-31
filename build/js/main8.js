73a3d729e0e8ddc98cf4cf5f2464328e797684ea
Samsonov Egeny <evgeniy.samsonov@sbermegamarket.ru>
2024-07-31 17:50:50 +0300

    done desktop

--- build/js/main8.js
+++ build/js/main8.js
@@ -54,17 +54,12 @@
 
 "use strict";
 (function () {
-  const togglers = document.querySelectorAll(".js-faq-toggler");
+  const togglers = document.querySelectorAll(".js-faq-item");
   if (!togglers.length) return;
 
   togglers.forEach((toggler) => {
     toggler.addEventListener("click", (event) => {
-      const target = event.currentTarget;
-      if (!target) return;
-      const content = target.nextElementSibling;
-      if (!content) return;
-      target.classList.toggle("active");
-      content.classList.toggle("active");
+      toggler.classList.toggle("show");
     });
   });
 })();
