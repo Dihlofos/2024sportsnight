bd43a516f3d5f135ce3a6dfc6705dc158af8d371
Samsonov Egeny <evgeniy.samsonov@sbermegamarket.ru>
2024-08-04 11:51:29 +0300

    done mobile

--- build/js/main8.js
+++ build/js/main8.js
@@ -3,7 +3,7 @@
   const slider = document.querySelector(".js-actions-slider");
   const vw = window.innerWidth;
   const sliderMargin = vw > 1024 ? 43 : 20;
-  const slidesPerView = vw > 744 ? 3 : 1;
+  const slidesPerView = vw >= 744 ? 3 : 1;
   if (!slider) return;
 
   new Swiper(`.js-actions-slider`, {
